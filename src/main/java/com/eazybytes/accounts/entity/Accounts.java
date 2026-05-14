a373be811b477d55af9ee9622a3fc255c5323787
visaldinuwara <visaldinuwara@gmail.com>
2026-05-14 20:46:01 +0530

    creating entity

---
+++ src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,37 @@
+package com.eazybytes.accounts.entity;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Accounts extends BaseEntity{
+    //@GeneratedValue(strategy = GenerationType.AUTO,generator="native")
+    //@GenericGenerator(name = "native",strategy = "native")  Depricated in recent spring versions
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+    @Column(name="account_number")
+    @Id
+    private Long accountNumber;
+    @Column(name="cloun_type")
+    private String columnType;
+    @Column(name="branch_address")
+    private String branchAddress;
+    
+}
